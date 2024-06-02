d6208bd5d1582426a89d6f05c23741cd1158b3b2
Kei-K23 <keiksl2301@gmail.com>
2024-06-02 08:23:43 +0630

    Add message and message type entity

---
+++ realtime-chat-app-backend/src/main/java/dev/kei/entity/Message.java
@@ -0,0 +1,17 @@
+package dev.kei.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Message {
+    private MessageType type;
+    private String content;
+    private String sender;
+    private String sessionId;
+}
---
+++ realtime-chat-app-backend/src/main/java/dev/kei/entity/MessageType.java
@@ -0,0 +1,7 @@
+package dev.kei.entity;
+
+public enum MessageType {
+    CHAT,
+    CONNECT,
+    DISCONNECT
+}
